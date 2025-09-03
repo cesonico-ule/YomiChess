df26d312adc48cba4f87bd722bbbedb90ecd5f89
cesonico-ule <cberf@unileon.es>
2025-09-04 00:13:09 +0200

    GitHub migration
    This is the first playable version of the project. It has already been fully developed offline within a local Unity setup, and this update is the migration of said version.
    It contains a chess board with a full set of playable pieces following the standard rules of chess. From now on, every additional functionality will be added directly here, including additional testing branches.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Dynamic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public GameObject chesspiece;
+
+    // Positions and team for each piece
+    private GameObject[,] positions = new GameObject[8, 8];
+    private GameObject[] playerBlack = new GameObject[16];
+	private GameObject[] playerWhite = new GameObject[16];
+
+    private string currentPlayer = "white"; // This might be useless or work to determine the player in a match against a bot
+
+    private bool gameOver = false;
+
+
+	// Start is called before the first frame update
+	void Start() {
+
+        playerWhite = new GameObject[] {
+            Create("white_king", 4, 0), Create("white_queen", 3, 0),
+            Create("white_rook", 0, 0), Create("white_rook", 7, 0),
+            Create("white_bishop", 2, 0), Create("white_bishop", 5, 0),
+			Create("white_knight", 1, 0), Create("white_knight", 6, 0),
+			Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1), Create("white_pawn", 3, 1),
+            Create("white_pawn", 4, 1), Create("white_pawn", 5, 1), Create("white_pawn", 6, 1), Create("white_pawn", 7, 1)
+		};
+
+		playerBlack = new GameObject[] {
+			Create("black_king", 4, 7), Create("black_queen", 3, 7),
+			Create("black_rook", 0, 7), Create("black_rook", 7, 7),
+			Create("black_bishop", 2, 7), Create("black_bishop", 5, 7),
+			Create("black_knight", 1, 7), Create("black_knight", 6, 7),
+			Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6), Create("black_pawn", 3, 6),
+			Create("black_pawn", 4, 6), Create("black_pawn", 5, 6), Create("black_pawn", 6, 6), Create("black_pawn", 7, 6)
+		};
+
+		// Set all positions on the position board
+		for (int i = 0; i < playerWhite.Length; i++) {
+			SetPosition(playerWhite[i]);
+			SetPosition(playerBlack[i]);
+		}
+
+		// Setup chess pieces
+		// Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+
+
+    }
+
+
+	public GameObject Create(string name, int x, int y) {
+		GameObject obj = Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+		Chessman cm = obj.GetComponent<Chessman>();
+		cm.name = name;
+		cm.SetXBoard(x);
+		cm.SetYBoard(y);
+		cm.Activate();
+		return obj;
+	}
+
+	public void SetPosition(GameObject obj) {
+		// Set the position of the chess piece on the board
+		Chessman cm = obj.GetComponent<Chessman>();
+		positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
+
+
+		/*
+		if (cm != null) {
+			cm.SetCoordinates();
+			// Update the positions array if needed
+			controller.GetComponent<Game>().SetPosition(obj);
+		}
+		*/
+	}
+
+	public void SetPositionEmpty (int x, int y) {
+		// Set the position of the chess piece on the board to empty
+		positions[x, y] = null;
+	}
+
+	public GameObject GetPosition(int x, int y) {
+		// Get the position of the chess piece on the board
+		return positions[x, y];
+	}
+
+	public bool PositionOnBoard(int x, int y) {
+		if (x < 0 || x > 7 || y < 0 || y > 7) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/MovePlate.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlate : MonoBehaviour
+{
+	public GameObject controller;
+	GameObject reference = null;
+
+	int matrixX;
+	int matrixY;
+
+	// False = Empty square, True = Threat
+	public bool attack = false;
+
+
+	public void Start() {
+		if (attack) {
+			// Change to red
+			gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 0.5f); ;
+		}
+	}
+
+	public void OnMouseUp() {
+		controller = GameObject.FindGameObjectWithTag("GameController");
+
+		if (attack)
+		{
+			GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
+
+			Destroy(cp);
+		}
+
+		controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(), reference.GetComponent<Chessman>().GetYBoard());
+
+		reference.GetComponent<Chessman>().SetXBoard(matrixX);
+		reference.GetComponent<Chessman>().SetYBoard(matrixY);
+		reference.GetComponent<Chessman>().SetCoordinates();
+
+		controller.GetComponent<Game>().SetPosition(reference);
+
+		reference.GetComponent<Chessman>().DestroyMovePlates();
+	}
+	
+
+	public void SetCoords(int x, int y) {
+		matrixX = x;
+		matrixY = y;
+	}
+
+	public void SetReference(GameObject obj) {
+		reference = obj;
+	}
+	public void GetReference(GameObject obj) {
+		reference = obj;
+	}
+
+}
